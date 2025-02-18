9a17871a7a647d5c869abefd46c2006ea1057150
MichaelCai2005 <78581242+MichaelCai2005@users.noreply.github.com>
2025-02-18 23:55:20 +0000

    Deploying to gh-pages from @ MichaelCai2005/MichaelCai2005.github.io@afd64bc8cd294876f5774975d2c1922fce4847ab 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,28 +9,21 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-lecture-notes",
+          title: "Lecture Notes",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/notes/";
+          },
+        },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "dropdown-projects",
-              title: "projects",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "";
-              },
-            },{id: "projects-project-1",
+        },{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {

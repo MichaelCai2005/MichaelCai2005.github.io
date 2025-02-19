9eb9c8c76c0990a117f6cb5994e4611ea565d772
MichaelCai2005 <78581242+MichaelCai2005@users.noreply.github.com>
2025-02-19 00:48:37 +0000

    Deploying to gh-pages from @ MichaelCai2005/MichaelCai2005.github.io@cf8bd20f8a7f24b18e6971b0914157e532bac84a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/notes/";
           },
-        },{id: "nav-projects",
-          title: "projects",
+        },{id: "nav-",
+          title: "",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
